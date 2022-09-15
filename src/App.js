4cc57965824e0ba43d9acac67fbc08c41881bb1c
Rhea Dieberger <rdieberger@gmail.com>
2022-09-15 18:14:13 +0200

    Display meme on website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { useState } from 'react';
-import Fetch from './Fetch';
+import MemeData from './MemeDatabase';
 
 export default function App() {
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const [memeImage, setMemeImage] = useState('');
+  const [topText, setTopText] = useState('Welcome');
+  const [bottomText, setBottomText] = useState('to meme world');
+  const [memeImage, setMemeImage] = useState('spongebob');
   return (
     <div>
-      <h1 className="headline">MEME GENERATOR</h1>
+      <h1 style={{ textAlign: 'center' }}>MEME GENERATOR</h1>
       <br />
       <label>
         Top Text
@@ -31,16 +31,50 @@ export default function App() {
       </label>
       <br />
       <label>
-        Meme Background Image
+        Meme Template
         <input
-          type="search"
+          list="memes"
           value={memeImage}
           onChange={(event) => {
-            setMemeImage(event.currentTarget.value);
+            setMemeImage(event.currentTarget.value.toLowerCase());
           }}
         />
+        <datalist id="memes">
+          <option>Keanu</option>
+          <option>Spongebob</option>
+          <option>Bender</option>
+          <option>Fry</option>
+          <MemeData />
+        </datalist>
       </label>
-      {/* <Fetch /> */}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          margin: 20,
+          padding: 20,
+        }}
+      >
+        {/* Image Element */}
+        {/* <img
+          data-test-id="meme-image"
+          alt="Meme"
+          src={`https://api.memegen.link/images/${memeImage}.jpg`}
+        /> */}
+        <img
+          data-test-id="meme-image"
+          alt="Meme"
+          src={`https://api.memegen.link/images/${memeImage}/${topText}/${bottomText}.jpg`}
+        />
+      </div>
     </div>
   );
 }
+
+// Bild von hier:
+// https://api.memegen.link/images/${memeImage}/${topText}/${bottomText}.jpg
+// const downloadAndSaveCustomMeme = async (url, folder, path) => {
+// const imgToGet = await fetch('https://api.memegen.link/images/bender/memes/memes_everywhere.jpg');
+// const arrayBuffer = await imgToGet.arrayBuffer();
+// const buffer = Buffer.from(arrayBuffer);
+// console.log(buffer)
